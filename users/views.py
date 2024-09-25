ca440dc6bf86de13a2c5908f85771351c7242cf5
Surendra-G <girisurendra321@gmail.com>
2024-09-25 23:20:29 +0545

    Added Setting Page

--- users/views.py
+++ users/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Profile  
+from django.contrib.auth.models import User
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 
+
+#Functionality for profile
 def profile(request):
     try:
         profile = request.user.profile
@@ -11,6 +16,7 @@ def profile(request):
         return redirect('create_profile')
     # Render the profile template or handle other logic
     return render(request, 'profile.html', {'profile': profile})
+
 
 def create_profile(request):
     if request.method == 'POST':
@@ -21,6 +27,8 @@ def create_profile(request):
         return redirect('profile')
     return render(request, 'create_profile.html')
 
+
+#Functionality for profile picture change
 @login_required
 def update_profile_picture(request):
     if request.method == 'POST':
@@ -39,7 +47,48 @@ def update_profile_picture(request):
         return redirect('profile')
     return redirect('profile')  
 
+
+
 # functionality for the setting 
 @login_required
-def users_setting( request):
-    return redirect('users_setting')
+def users_setting(request):
+    if request.method == 'POST':
+        # Get data from the form
+        first_name = request.POST.get('firstname')
+        last_name = request.POST.get('lastname')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = request.user
+
+        # Update firstname,lastname, username and email
+        if first_name:
+            user.first_name = first_name
+        if last_name:
+            user.last_name = last_name
+        if username:
+            user.username = username
+        if email:
+            user.email = email
+
+         # Update password using Django's PasswordChangeForm for validation
+        if password:
+            form = PasswordChangeForm(user=user, data=request.POST)
+            if form.is_valid():
+                form.save()
+                update_session_auth_hash(request, user)  # Prevents logout after password change
+                messages.success(request, 'Password updated successfully!')
+            else:
+                messages.error(request, 'Error updating password: ' + str(form.errors))
+                return redirect('users_setting')
+
+        user.save()
+
+        messages.success(request, 'Settings updated successfully!')
+        return redirect('users_setting')
+    
+    # Render the settings page
+    return render(request, 'setting.html', {
+        'user': request.user,
+    })
